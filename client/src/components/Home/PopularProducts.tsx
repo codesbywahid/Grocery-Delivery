1328c94dca72c116a4531b74406ac351baee54e5
Abdul Wahid <aw3559113@gmail.com>
2026-09-02 15:54:59 +0500

    Popular Products Added

--- client/src/components/Home/PopularProducts.tsx
+++ client/src/components/Home/PopularProducts.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import type { Product } from "../../types"
 import { dummyProducts } from "../../assets/assets"
-import { ArrowRightIcon, Link } from "lucide-react"
+import { ArrowRightIcon } from "lucide-react"
+import ProductCard from "../ProductCard"
 
 const PopularProducts = () => {
     const [products, setProducts] = useState<Product[]>([])
@@ -10,15 +12,17 @@ const PopularProducts = () => {
   return (
     <section className="pb-16">
         <div className="max-w-7xl mx-auto">
-            <div className="flex-items-centr justify-between mb-8">
+            <div className="flex items-center justify-between mb-8">
                 <h2 className="text-2xl font-semibold">Popular Products</h2>
                 <p className="text-sm text-app-text-light mt-1">Top-rated products this season</p>
             </div>
-            <Link to='/products' className=" text-sm font-semibold text-app-orange hover:text-app-orange-dark flex items-center gap-1 transition colors"> View all <ArrowRightIcon />
+            <Link to='/products' className=" text-sm font-semibold text-app-orange hover:text-app-orange-dark flex items-center gap-1 transition-colors"> View all <ArrowRightIcon />
             </Link>
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 xl:gap-8">
-            <p>Products List</p>
+            {products.map((product)=>(
+            <ProductCard key={product._id} product={product} />
+            ))}
 
         </div>
     </section>
--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import type {Product} from "../types";
+import { Plus, Star } from "lucide-react";
 interface Props{
     product: Product;
 }
@@ -8,10 +9,10 @@ const ProductCard = ({product}:Props) => {
     const {addToCart} = {addToCart: (_data :any)=>{}}
     const navigate = useNavigate()
   return (
-    <div className="bg-white rounded-2xl overflow-hidden shadow hover:shadow-md transition-all duration-300 group animate-fate-in cursor-pointer" onClick={()=> navigate ('/products/${product_id}')}>
+    <div className="bg-white rounded-2xl overflow-hidden shadow hover:shadow-md transition-all duration-300 group animate-fade-in cursor-pointer" onClick={()=> navigate(`/products/${product._id}`)}>
         {/* {Image} */}
         <div className="relative aspect-square overflow-hidden">
-            <img src={product.image} alt={product.image} className="w-full h-full object cover p-4 group-hover:p-2 transition-all duration-300" />
+            <img src={product.image} alt={product.image} className="w-full h-full object-cover p-4 group-hover:p-2 transition-all duration-300" />
 
             {/* {Badge} */}
             <div>
@@ -19,7 +20,28 @@ const ProductCard = ({product}:Props) => {
              <span className="px-2 py-0.5 text-[10px] font-semibold uppercase bg-app-orange text-white rounded-full">   {product.discount}% OFF
              </span>}
             </div>
-
+        </div>
+        {/* {Info} */}
+        <div className="p-3.5 text-zinc-700">
+            <h3 className="text-sm leading-snug mb-1.5 line-clamp-2">{product.name}
+            </h3>
+            {/* {Rating} */}
+            <div className="flex items-center gap-1 mb-2">
+                <Star className="size-3 text-app-warning fill-app-warning"/>
+                <span>{product.rating}</span>
+                <span className="text-xs text-app-text-light">({product.reviewCount})</span>
+            </div>
+            {/* {Price + Add} */}
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-1 truncate">
+                    <span className="text-base font-medium">{currency}{product.price.toFixed(1)}</span>
+                    <span className="text-xs text-app-text-light block">{product.unit}</span>
+                    {product.originalPrice > product.price && <span className="text-xs text-app-text-light line-through ml-1.5">{currency}{product.originalPrice.toFixed(1)}</span>}
+                </div>
+                <button onClick={(e)=>{e.stopPropagation(); addToCart(product)}} className="size-7 rounded-full bg-app-orange text-white flex-center shrink-0 hover:bg-app-orange-dark transition-colors active:scale-95">
+                    <Plus className="size-3.5"/>
+                </button>
+            </div>
         </div>
     </div>
   )
