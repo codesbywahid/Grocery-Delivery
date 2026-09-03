c8f61f1145d27c75729c4f0c736d54d46ee0567d
Abdul Wahid <aw3559113@gmail.com>
2026-09-03 23:14:39 +0500

    Added Footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { BikeIcon, Link } from "lucide-react"
+import { footerData } from "../assets/assets"
+
+const Footer = () => {
+  return (
+    <footer className="bg-app-green text-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            {/* Top  */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                {/* Brand */}
+                <div>
+                    <Link to='/' className="flex items-center gap-2 mb-4">
+                    <BikeIcon className="size-6 text-white" />
+                    <span className="text-xl font-semibold">{footerData.brand.name}</span>
+                    </Link>
+                </div>
+                <p className="text-sm text-white/70 mb-4">{footerData.brand.description}</p>
+                <div className="flex gap-3">
+
+                </div>
+
+            </div>
+            {/* Bottom */}
+            <div>
+
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
