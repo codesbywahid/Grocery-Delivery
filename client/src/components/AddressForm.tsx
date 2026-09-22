6b5cb28c5455379f85b7076c4c26b051d496819f
Abdul Wahid <aw3559113@gmail.com>
2026-09-22 22:09:00 +0500

    Added zip code and set as default

--- client/src/components/AddressForm.tsx
+++ client/src/components/AddressForm.tsx
@@ -39,11 +39,51 @@ const AddressForm = ({
 
           {/* Form Input fields */}
           <div className="space-y-4">
+            <div>
             <label className="block text-sm font-medium text-app-green mb-1.5">
               Label
             </label>
+            <input type="text" placeholder="Home,Work, etc." required className="w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none" value={form.label} onChange={(e)=> setForm({...form,label:e.target.value})}/>
+          </div>
 
-            <input type="text" />
+            <div>
+            <label className="block text-sm font-medium text-app-green mb-1.5">
+              Street Address
+            </label>
+            <input type="text" required className="w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none" value={form.address} onChange={(e)=> setForm({...form,address:e.target.value})}/>
+          </div>
+          <div className="grid grid-cols-2 gap-3">
+             <div>
+              <label className="block text-sm font-medium text-app-green mb-1.5">
+              City
+            </label>
+            <input type="text" required className="w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none" value={form.city} onChange={(e)=> setForm({...form,city:e.target.value})}/>
+          </div>
+
+          <div>
+              <label className="block text-sm font-medium text-app-green mb-1.5">
+              State
+            </label>
+            <input type="text" required className="w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none" value={form.state} onChange={(e)=> setForm({...form,state :e.target.value})}/>
+          </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-3">
+            <div>
+              <label className="block text-sm font-medium text-app-green mb-1.5">
+              ZIP code
+            </label>
+            <input type="text" required className="w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none" value={form.zip} onChange={(e)=> setForm({...form,zip :e.target.value})}/>
+            </div>
+
+            <div className="flex items-end pb-1">
+              <label className="block text-sm font-medium text-app-green mb-1.5">
+              <span className="text-sm text-app-text">Set as default</span>
+            </label>
+            </div>
+
+
+          </div>
           </div>
 
           {/* Submit Button */}
