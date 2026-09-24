c840e083e2baa9fc840bde0c5dfffc823e4b87b4
Abdul Wahid <aw3559113@gmail.com>
2026-09-24 23:30:23 +0500

    Adding Components

--- client/src/pages/CheckOut.tsx
+++ client/src/pages/CheckOut.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import { useCart } from "../context/CartContext";
+import { dummyAddressData } from "../assets/assets";
+import type { Address } from "../types";
 
 const CheckOut = () => {
+  const navigate = useNavigate()
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || '$';
+
+  const {items,cartTotal} = useCart()
+  const{user}={user:{addresses: dummyAddressData}}
+
+
+
+  const [step,setStep] = useState("address")
+  const [loading,setLoading]=useState(false)
+  const [address,setAddress] = useState<Address>({
+    _id:"",
+    label:"Home",
+    address:"",
+    city:"",
+    state:"",
+    zip:"",
+    isDefault:false,
+    lat:0,
+    lng:0
+  })
+
+  const [paymentMethod,setPaymentMethod]=useState('card')
+  const deliveryFee = cartTotal >20?0:1.99;
+  const tax=cartTotal * 0.08;
+  const total = cartTotal + deliveryFee +tax;
+
+
+
   return (
     <div>CheckOut</div>
   )
