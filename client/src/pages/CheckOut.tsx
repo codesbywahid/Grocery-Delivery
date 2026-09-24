a548d6ade012284cf517b4446b5911d7d952f251
Abdul Wahid <aw3559113@gmail.com>
2026-09-24 23:37:39 +0500

    Added address payment and review

--- client/src/pages/CheckOut.tsx
+++ client/src/pages/CheckOut.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 import { useCart } from "../context/CartContext";
 import { dummyAddressData } from "../assets/assets";
 import type { Address } from "../types";
-
+import { CheckIcon, CreditCardIcon, MapPinIcon } from "lucide-react";
 const CheckOut = () => {
   const navigate = useNavigate()
   const currency = import.meta.env.VITE_CURRENCY_SYMBOL || '$';
@@ -32,7 +32,15 @@ const CheckOut = () => {
   const tax=cartTotal * 0.08;
   const total = cartTotal + deliveryFee +tax;
 
-
+  const steps: {key:string;label:string;icon:typeof MapPinIcon}[]=[
+    {key:"address",label:"Address",icon:MapPinIcon},
+    {key:"payment",label:"Payment",icon:CreditCardIcon},
+    {key:"review",label:"Review",icon:CheckIcon},
+  ]
+  const handlePlaceOrder = async()=>{
+    setLoading(true)
+    navigate("/orders")
+  }
 
   return (
     <div>CheckOut</div>
