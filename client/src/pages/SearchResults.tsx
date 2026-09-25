0c2ad2e5490e49ae96eb6b72bf030c8ae7d336c7
Abdul Wahid <aw3559113@gmail.com>
2026-09-25 16:54:26 +0500

    Adding search results for products

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -1,8 +1,28 @@
-
+import { useEffect, useState } from "react"
+import type { Product } from "../types"
+import { useSearchParams } from "react-router-dom"
+import { dummyProducts } from "../assets/assets"
 const SearchResults = () => {
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+  const [searchParams] = useSearchParams()
+  const query = searchParams.get('q') || "";
+  useEffect(() => {
+    if (!query) return;
+    setLoading(true)
+    setProducts(
+      dummyProducts.filter((p: any) =>
+        p.name.toLowerCase().includes(query.toLowerCase())))
+    setLoading(false)
+  }, [query])
+
   return (
-    <div>SearchResults</div>
+    <div className="min-h-screen bg-app-cream">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        {/* BreadCrumbs */}
+
+      </div>
+    </div>
   )
 }
-
 export default SearchResults
